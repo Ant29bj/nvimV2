8ad6e0d429f71869f1090407b4da035612e8d759
Ant29bj <bojorquez29an@gmail.com>
2023-01-24 22:57:23 -0700

    lsp configurado

--- lua/sekai/plugins/nvim-cmp.lua
+++ lua/sekai/plugins/nvim-cmp.lua
@@ -1,49 +1,80 @@
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
-  return 
-end  
+	return
+end
 
 local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status then
-  print('luasnip no funciona')
-  return
+	print("luasnip no funciona")
+	return
 end
 
 local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
-  return  
+	return
 end
 
-require('luasnip/loaders/from_vscode').lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["UP"] = cmp.mapping.select_prev_item(), 
-    ["<Tab>"] = cmp.mapping.select_next_item(), 
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), 
-    ["<C-e>"] = cmp.mapping.abort(), 
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  sources  = cmp.config.sources({
-    {name = "nvim_lsp"},
-    {name = "luasnip"},
-    {name = "buffer"},
-    {name = "path"},
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      maxwidth = 50,
-      ellipsis_char = "...",
-    }),
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["UP"] = cmp.mapping.select_prev_item(),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	}),
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.menu = ({
+				nvim_lsp = "〄",
+				nvim_lua = "",
+				luasnip = "𝓢",
+				buffer = "",
+			})[entry.source.name]
+			vim_item.kind = ({
+				Text = "",
+				Method = "",
+				Function = "",
+				Constructor = "",
+				Field = "",
+				Variable = "",
+				Class = "",
+				Interface = "ﰮ",
+				Module = "",
+				Property = "",
+				Unit = "",
+				Value = "",
+				Enum = "",
+				Keyword = "",
+				Snippet = "﬌",
+				Color = "",
+				File = "",
+				Reference = "",
+				Folder = "",
+				EnumMember = "",
+				Constant = "",
+				Struct = "",
+				Event = "",
+				Operator = "ﬦ",
+				TypeParameter = "",
+			})[vim_item.kind]
+			return vim_item
+		end,
+	},
 })
-
